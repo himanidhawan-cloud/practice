aee27564331e104155d601d3ddfd8dbff15340d7
himanidhawan <himanidhawan@LT-MBP-120.local>
2026-06-15 19:02:06 +0530

    python practice 2

---
+++ ifelse.py
@@ -0,0 +1,12 @@
+age =int(input("enter age"))
+if(age>=18):
+    print("you are adult")
+else:
+    print("you are minor")
+
+
+num =int(input("enter a number"))
+if (num%2==0):
+    print("even")
+else:
+    print("odd")
