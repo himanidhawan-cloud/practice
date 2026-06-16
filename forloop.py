6ae1a6d5a579008326e3fc71d8568af43bb2ff29
himanidhawan <himanidhawan@LT-MBP-120.local>
2026-06-16 13:15:47 +0530

    iteration and basic for loop

---
+++ forloop.py
@@ -0,0 +1,35 @@
+#list
+list1=["hello","welcome","user"]
+for i in list1:
+    print(i)
+
+#dictionaries
+dictionary={"first":"ravi","second":"seema","third":"meena"}
+for i in dictionary:
+    print(i)
+
+for k,l in dictionary.items():
+    print("position is ",k," name is ", l)
+
+print(dictionary.keys())
+print(dictionary.values())
+
+#tuples
+tu=(6,7,8,9,10)
+for i in tu:
+    print(i)
+
+#sets
+set1={'mon','tue','wed'}
+for i in set1:
+    print(i)
+
+#
+for i in range(0,6):
+    print(i)
+
+for i in range(0,100,5):
+    print(i)
+
+for i in range (100,0,-5):
+    print(i)
